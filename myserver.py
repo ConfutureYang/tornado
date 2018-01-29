b04290712f0b790503b86b296442fb6eed749c71
yangfeihu <yangfeihu@tigerbrokers.com>
2018-01-29 10:10:35 +0800

    None

---
+++ myserver.py
@@ -0,0 +1,22 @@
+#coding:utf-8
+from tornado import ioloop
+from tornado  import web
+import time
+import json
+
+class MainHandler(web.RequestHandler):
+	def get(self):
+		y = self.get_argument("y")
+		y = json.loads(y)
+		print y
+		print type(y)
+		self.write("Hello Tornado")
+
+def make_app():
+	return web.Application([(r"/",MainHandler),])
+
+
+if __name__ == "__main__":
+	app = make_app()
+	app.listen(8888)
+	ioloop.IOLoop.current().start()
